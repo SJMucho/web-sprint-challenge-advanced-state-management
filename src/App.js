386014d9985bcaffd9cce8d12bba963aaf096cdd
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-06-27 15:35:11 -0400

    smurfAction connected to APP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from "react";
-
-import AddForm from './components/AddForm';
-import SmurfList from './components/SmurfList';
-import Header from './components/Header';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import AddForm from "./components/AddForm";
+import SmurfList from "./components/SmurfList";
+import Header from "./components/Header";
+import { smurfAction } from "./actions/index";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Header />
 
-        <main>
-          <SmurfList/>
-          <AddForm/>
-        </main>
-      </div>
-    );
-  }
-}
+//         <main>
+//           <SmurfList />
+//           <AddForm />
+//         </main>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+const App = (props) => {
+  console.log(props);
+
+  useEffect(() => {
+    props.smurfAction();
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+
+      <main>
+        <SmurfList />
+        <AddForm />
+      </main>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    state: state,
+  };
+};
+
+export default connect(mapStateToProps, { smurfAction })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,19 @@
 import axios from "axios";
-import { initialState } from "../reducers/index";
-export const FETCH_SMURF_START = "FETCH_SMURF_START";
+// import { initialState } from "../reducers/index";
+export const FETCH_SMURF_LOADING = "FETCH_SMURF_LOADING";
 export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
 export const FETCH_SMURF_FAILURE = "FETCH_SMURF_FAILURE";
 
+export const ADD_SMURF = "ADD_SMURF";
+export const TOGGLE_SMURF = "TOGGLE_SMURF";
+
 export const smurfAction = () => (dispatch) => {
-  dispatch({ type: FETCH_SMURF_START });
+  dispatch({ type: FETCH_SMURF_LOADING });
   axios
     .get(`http://localhost:3333/smurfs`)
     .then((res) => {
       console.log(res);
-      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data.smurf });
+      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
     })
 
     .catch((err) => {
@@ -18,6 +21,20 @@ export const smurfAction = () => (dispatch) => {
       dispatch({ type: FETCH_SMURF_FAILURE, payload: err });
     });
 };
+
+// export function addNewSmurf() {
+//   return {
+//     type: ADD_SMURF,
+//     payload: newSmurf,
+//   };
+// }
+
+// export function toggleSmurf(index) {
+//   return {
+//     type: TOGGLE_SMURF,
+//     payload: index,
+//   };
+// }
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
